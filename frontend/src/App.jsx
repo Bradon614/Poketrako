75d4010538c06a75442908adab07e3fb1284144b
Christiano Bradon <christianobradon@gmail.com>
2025-09-02 15:02:20 +0300

    add Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './pages/Intro';
+import Signup from './pages/signup';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Intro/>}/>
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Signup.css';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  return (
+    <div className="signup-container">
+      <h2>Signup</h2>
+      <form>
+        <input type="text" placeholder="Username" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">SIGN UP</button>
+        <p>
+          Already have an account? <Link to="/">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
